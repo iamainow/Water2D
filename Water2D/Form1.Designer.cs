9e7ad8d7ea3f6632332f2be0262962d5363ee657
iamainow <iamainow@gmail.com>
2025-06-13 23:12:52 +0400

    add project

---
+++ Water2D/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace Water2D
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components=null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing&&(components!=null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._image=new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this._image)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// _image
+			// 
+			this._image.BackColor=System.Drawing.Color.White;
+			this._image.Dock=System.Windows.Forms.DockStyle.Fill;
+			this._image.Location=new System.Drawing.Point(0,0);
+			this._image.Name="_image";
+			this._image.Size=new System.Drawing.Size(534,412);
+			this._image.TabIndex=0;
+			this._image.TabStop=false;
+			this._image.Click+=new System.EventHandler(this._image_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions=new System.Drawing.SizeF(6F,13F);
+			this.AutoScaleMode=System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize=new System.Drawing.Size(534,412);
+			this.Controls.Add(this._image);
+			this.MaximizeBox=false;
+			this.Name="Form1";
+			this.Text="Form1";
+			this.Shown+=new System.EventHandler(this.DO);
+			((System.ComponentModel.ISupportInitialize)(this._image)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox _image;
+	}
+}
+
---
+++ Water2D/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace Water2D
+{
+	public partial class Form1:Form
+	{
+		Point prev;
+		static float ddump=0.01F;
+		static float dumping=1+ddump;
+		int FPS=0;
+		DateTime FPSTime=DateTime.Now;
+		public static int N=400;
+		float[] source=new float[N];
+		float[] dest=new float[N];
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void DO(object sender,EventArgs e)
+		{
+			while(true)
+			{
+				var P=_image.PointToClient(Cursor.Position);
+				if(prev!=new Point(0,0))
+				{
+					int dx=Math.Abs(P.X-prev.X);
+					var R=new Random();
+					Wave(P.X*N/550,(int)(N*0.05F),dx*(P.Y-200)*ddump*0.1F);
+				}
+				prev=P;
+				//
+				FPS++;
+				if((DateTime.Now-FPSTime).TotalSeconds>=1)
+				{
+					this.Text=FPS.ToString();
+					FPS=0;
+					FPSTime=DateTime.Now;
+				}
+				float[] dest_new=new float[N];
+				dest[0]=2*(source[1])-dest[0];
+				dest[0]/=dumping;
+				dest[N-1]=2*(source[N-2])-dest[N-1];
+				dest[N-1]/=dumping;
+				for(int i=1;i<N-1;i++)
+				{
+					dest[i]=(source[i-1]+source[i+1])-dest[i];
+				}
+				for(int i=1;i<N-1;i++)
+				{
+					dest[i]=(dest[i-1]+dest[i+1])/2;
+				}
+				for(int i=1;i<N-1;i++)
+				{
+					dest[i]=dest[i]/dumping;
+				}
+				Bitmap BM=new Bitmap(_image.Size.Width,_image.Size.Height);
+				float ds=550F/N;
+				using(Graphics G=Graphics.FromImage(BM))
+				{
+					for(int i1=1;i1<N;i1++)
+					{
+						G.DrawLine(Pens.Black,(int)((i1-1)*ds),200+dest[i1-1],(int)((i1)*ds),200+dest[i1]);
+					}
+				}
+				if(_image.BackgroundImage!=null)
+				{
+					_image.BackgroundImage.Dispose();
+				}
+				_image.BackgroundImage=BM;
+				var R2=source;
+				source=dest;
+				dest=R2;
+				Application.DoEvents();
+				System.Threading.Thread.Sleep(0);
+			}
+		}
+		private void _image_Click(object sender,EventArgs e)
+		{
+			var P=_image.PointToClient(Cursor.Position);
+			var R=new Random();
+			Wave(P.X*N/550,(int)(N*1F),(P.Y-200)/5);
+		}
+		private void Wave(int C,int DR,float H)
+		{
+			if(H!=0)
+			{
+				for(int i1=0;i1<N;i1++)
+				{
+					if(Math.Abs(C-i1)<=DR)
+					{
+						source[i1]+=(float)(H*Math.Cos(((1.5708F*Math.Abs(C-i1))/DR)));
+					}
+				}
+			}
+		}
+	}
+}
